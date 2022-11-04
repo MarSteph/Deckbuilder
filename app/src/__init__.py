4c7cde5f5c261b2c64d55b118b35956b1e52a50f
Marley Stephens <marley.stephens@gmail.com>
2022-11-04 18:09:35 -0500

    init.py changes

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='postgresql://postgres@localhost:5432/mtg_deckbuilder',
+        SQLALCHEMY_DATABASE_URI='postgresql://postgres@pg:5432/mtg_deckbuilder',
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
         SQLALCHEMY_ECHO=True
     )
